a1a9542bd16d32779e6b0d22971d8d51731f97c2
ccza <a125012239@gmail.com>
2022-07-20 23:39:32 +0800

    axiosFail

--- JustGo/JustGo/Controllers/HomeController.cs
+++ JustGo/JustGo/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using JustGo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 namespace JustGo.Controllers
@@ -26,10 +27,34 @@ namespace JustGo.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult axiosTest(string request)
+        {
+            if (request == null)
+            {
+                return Error();
+            }
+            Request jObj = JsonConvert.DeserializeObject<Request>(request)??new Request();
+            if (jObj.start < 1 ||jObj.quantity<1)
+            {
+                return Error();
+            }
+            Console.WriteLine(jObj.start);
+            Console.WriteLine(jObj.quantity);
+            return Json(_pwr.getPlace(jObj.start, jObj.quantity));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
+    public class Request {
+        public int start { get; set;}
+        public int quantity { get; set;}
+    }
+
 }

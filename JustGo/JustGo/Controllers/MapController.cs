1d031ca7587bb072e3ac975d0ba3143385028a60
JuYinMSI <ohruin@gmail.com>
2022-07-22 11:48:23 +0800

    GoogleMapApi on C# with JavaScript

---
+++ JustGo/JustGo/Controllers/MapController.cs
@@ -0,0 +1,96 @@
+﻿using JustGo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustGo.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: MapController
+        readonly IPlaceWeatherRepostiory _pwr;
+        public MapController(IPlaceWeatherRepostiory rwp)
+        {
+            _pwr = rwp;
+        }
+
+        public ActionResult Index()
+        {
+            //string[] a = new string[]();
+            //string[] a = new string[] { "a", "a" };
+            //string[] b = new string[] { "高雄市" };
+            //string[] c = new string[] { };
+            //int[] d = new int[] { 1, 2, 3 };
+
+            return View();
+        }
+
+        // GET: MapController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MapController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MapController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MapController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MapController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MapController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MapController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- JustGo/JustGo/Program.cs
+++ JustGo/JustGo/Program.cs
@@ -15,10 +15,10 @@ var TravelPssP = builder.Configuration.GetConnectionString("TravelPssP");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddTransient<IDbConnection>(db => new SqlConnection(TravelWindows));
+builder.Services.AddTransient<IDbConnection>(db => new SqlConnection(TravelPssP));
 builder.Services.AddTransient<IPlaceWeatherRepostiory, PlaceWeatherRepostiory>();
 
-builder.Services.AddDbContext<TravelContext>(o => o.UseSqlServer(TravelWindows));
+builder.Services.AddDbContext<TravelContext>(o => o.UseSqlServer(TravelPssP));
 //連線字串替換
 //地端連線字串TravelWindows,雲端連線字串TravelPssP
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();

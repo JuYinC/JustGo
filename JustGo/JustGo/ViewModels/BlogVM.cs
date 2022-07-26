a5376ec33c9a8ea91d3aa04962a57b3bb8f69ae6
cc <pinjuw@gmail.com>
2022-07-26 11:51:43 +0800

    createBlogVM

--- JustGo/JustGo/ViewModels/BlogVM.cs
+++ JustGo/JustGo/ViewModels/BlogVM.cs
@@ -2,5 +2,30 @@
 {
     public class BlogVM
     {
+        public int BlogId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserImage { get; set; }
+        public string Title { get; set; }
+        public string Describe { get; set; }
+        public string CoverImageName { get; set; }
+        public int? Like { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public ICollection<BlogDetailsVM> DetailsVMs { get; set; }
+    }
+
+    public partial class BlogDetailsVM
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndtTime { get; set; }
+        public int PlaceId { get; set; }
+        public string P_Name { get; set; }
+        public string P_tel { get; set; }
+        public string P_Add { get; set; }
+        public string Describe { get; set; }
+        public IList<string> Images { get; set; }
+        public double? Score { get; set; }
     }
 }

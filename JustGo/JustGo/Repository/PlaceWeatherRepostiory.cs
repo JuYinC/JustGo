49f898f0f3c5faa47bde3a6eb97e6a7eaf0b721a
cc <pinjuw@gmail.com>
2022-07-28 11:52:12 +0800

    Range_10KM

--- JustGo/JustGo/Repository/PlaceWeatherRepostiory.cs
+++ JustGo/JustGo/Repository/PlaceWeatherRepostiory.cs
@@ -22,7 +22,7 @@ namespace JustGo.Repository
 
         public ICollection<Place> getPlace(SelectPlaceVM vm)
         {
-            var getPlace = _con.Query<Place>("select * from fn_selePlaceDistance(@Lat,@Lng,25)",vm).ToList();            
+            var getPlace = _con.Query<Place>("select * from fn_selePlaceDistance(@Lat,@Lng,10)",vm).ToList();            
             Console.WriteLine(getPlace.GetType());
 
             return getPlace;

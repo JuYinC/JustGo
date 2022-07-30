5c6fc2715584410903829f1e25358689c3863904
ccza <a125012239@gmail.com>
2022-07-30 15:28:06 +0800

    Searchfeng

--- JustGo/JustGo/Controllers/SearchController.cs
+++ JustGo/JustGo/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using JustGo.Repository;
 using Microsoft.AspNetCore.Mvc;
+using JustGo.ViewModels;
 
 namespace JustGo.Controllers
 {
@@ -16,8 +17,10 @@ namespace JustGo.Controllers
         }
 
         [HttpPost]
-        public IActionResult testSearch()
+        public IActionResult testSearch([FromBody]SelectPlaceVM vm)
         {
+            Console.WriteLine(vm.selectCounty.Length);
+            Console.WriteLine(vm.selectAcitivity.Length);
             return Json(_schedule.selectUserSchedule("862c02ac-67e1-461f-ac15-74b166c0a1e4"));
         }
     }

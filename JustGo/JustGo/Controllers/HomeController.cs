1646b5ad0dd8c359b90eb8a2d158d8a70de09a23
syl w <cba50419@gmail.com>
2022-08-05 16:57:01 +0800

    format and images for editing and showing blogs

--- JustGo/JustGo/Controllers/HomeController.cs
+++ JustGo/JustGo/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace JustGo.Controllers
 
         public IActionResult Blog()
         {
-            return View("EditBlog");
+            return View("Blog");
         }
 
         public IActionResult EditBlog()

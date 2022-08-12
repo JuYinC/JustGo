d5e593e5aaaf95ab7bde6c579d7ddbbdde20fc9b
俊彥 林 <tfr52295@gmail.com>
2022-08-12 10:53:48 +0800

    8/12

---
+++ JustGo/JustGo/Controllers/register_.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JustGo.Controllers
+{
+    public class register_ : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
